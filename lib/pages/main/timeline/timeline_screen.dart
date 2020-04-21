26a7969947ed6ddd5d8e5c1882d79203dab49ca4
Tomáš Novák <tom.nov96@gmail.com>
2020-04-21 22:08:08 +0200

    Table Calendar implemented

--- lib/pages/main/timeline/timeline_screen.dart
+++ lib/pages/main/timeline/timeline_screen.dart
@@ -19,45 +19,87 @@ class _TimelineScreenState extends State<TimelineScreen> {
     _calendarController = CalendarController();
   }
 
+  void goToToday() {
+    print("goToToday");
+    setState(() {
+      _calendarController.setSelectedDay(DateTime.now(), animate: true);
+    });
+  }
+
+  void someDaySelected(DateTime date) {
+    print("selected date: $date");
+    setState(() {
+      _calendarController.setSelectedDay(date, animate: true);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          children: <Widget>[
-            Text(
-              "Daily ",
-              style: TextStyle(color: Colors.black),
-            ),
-            Text(
-              "Timeline",
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 17),
+            ),
             TableCalendar(
               calendarController: _calendarController,
               initialCalendarFormat: CalendarFormat.week,
               headerVisible: false,
-              formatAnimation: FormatAnimation.slide,
+              availableGestures: AvailableGestures.horizontalSwipe,
+              onDayLongPressed: (date, events) {
+                goToToday();
+              },
+              onDaySelected: (date, events) {
+                someDaySelected(date);
+              },
               startingDayOfWeek: StartingDayOfWeek.monday,
               daysOfWeekStyle: DaysOfWeekStyle(
+                weekdayStyle: TextStyle().copyWith(color: Colors.black),
                 weekendStyle: TextStyle().copyWith(color: primaryColor),
               ),
-              builders: CalendarBuilders(),
+              builders: CalendarBuilders(
+                todayDayBuilder: (context, date, events) =>
+                    Container(
+                      height: 5,
+                      margin: EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: primaryColor.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                selectedDayBuilder: (context, date, events) =>
+                    Container(
+                      height: 5,
+                      margin: EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+              ),
               calendarStyle: CalendarStyle(
+                  weekdayStyle: TextStyle().copyWith(color: Colors.black),
                   weekendStyle: TextStyle().copyWith(color: primaryColor),
-                  todayColor: primaryColor.withOpacity(0.2),
-                  todayStyle: TextStyle(color: Colors.black),
-                  selectedColor: primaryColor,
-                  outsideDaysVisible: false),
+                  outsideDaysVisible: true,
+                  outsideStyle: TextStyle().copyWith(color: Colors.black26),
+                  outsideWeekendStyle: TextStyle()
+                      .copyWith(color: primaryColor.withOpacity(0.26))),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.red,
+              ),
             )
           ],
         ),
